e46d58e182f0f33ae945e17d1af0e946fc75f568
longph <longphamduy2002@gmail.com>
2023-02-20 01:30:13 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿Random rand = new Random();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at
+// https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+var quizzes = new List<Quiz> {  };
+
+quizzes.Add(new Quiz("Who created Svelte?",
+    new List<string> { "Rich Harris", "Linus Torsvald", 
+        "Mark Zuckerberg", "Ada Lovelace" }, "Rich Harris"));
+
+app.MapGet("/quiz", () =>
+{
+    return new Quiz(quizzes[rand.Next(quizzes.Count)]);
+})
+.WithName("GetRandomQuiz");
+
+app.Run();
+
+internal record Quiz(string? quiz, List<string> options, string ans)
+{
+    public Quiz(Quiz _quiz) {
+        quiz = _quiz.quiz;
+        options = _quiz.options;
+        ans = _quiz.ans;
+    }
+}
