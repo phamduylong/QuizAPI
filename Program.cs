c1f9593ea2530db8117e5da51430c219aed28e83
longph <longphamduy2002@gmail.com>
2023-02-21 17:29:45 +0200

    class serialization works

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Random rand = new Random();
+﻿using Newtonsoft.Json;
+Random rand = new Random();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,25 +20,79 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var quizzes = new List<Quiz> {  };
+var quizzes = new List<Quiz> { };
 
-quizzes.Add(new Quiz("Who created Svelte?",
-    new List<string> { "Rich Harris", "Linus Torsvald", 
-        "Mark Zuckerberg", "Ada Lovelace" }, "Rich Harris"));
+var quiz1 = new Quiz("Who created Svelte?",
+    new List<string> { "Rich Harris", "Linus Torsvald",
+        "Mark Zuckerberg", "Ada Lovelace" }, AnswerType.str, "Rich Harris");
+
+var quiz2 = new Quiz("What version of Svelte are we in right now?", new List<string> { "0", "1", "2", "3" }, AnswerType.num, 3);
+
+quizzes.Add(quiz1);
+quizzes.Add(quiz2);
 
 app.MapGet("/quiz", () =>
 {
-    return new Quiz(quizzes[rand.Next(quizzes.Count)]);
+    var randRes = new Quiz(quizzes[rand.Next(quizzes.Count)]);
+    Console.WriteLine(randRes.ToString());
+    Console.WriteLine(randRes.GetType().Name);
+    var json_quiz = JsonConvert.SerializeObject(quizzes);
+    return json_quiz;
 })
-.WithName("GetRandomQuiz");
+.WithName("quiz");
 
 app.Run();
 
-internal record Quiz(string? quiz, List<string> options, string ans)
+enum AnswerType
 {
-    public Quiz(Quiz _quiz) {
-        quiz = _quiz.quiz;
+    str,
+    num,
+    binary
+
+}
+
+class Quiz
+{
+    // JSONProperty is need for serialization of private class members
+    [JsonProperty("title")]
+    private string title { get; set; }
+
+    [JsonProperty("options")]
+    private List<string> options { get; set; }
+
+    [JsonProperty("answertype")]
+    private AnswerType ansType { get; set; }
+
+    [JsonProperty("answer")]
+    private object ans { get; set; }
+
+    public Quiz()
+    {
+        title = "";
+        options = new List<string>();
+        ansType = AnswerType.str;
+        ans = "";
+    }
+
+    public Quiz(string _title, List<string> _options, AnswerType _ansType, object _ans)
+    {
+        title = _title;
+        options = _options;
+        ansType = _ansType;
+        ans = _ans;
+    }
+
+    public Quiz(Quiz _quiz)
+    {
+        title = _quiz.title;
         options = _quiz.options;
+        ansType = _quiz.ansType;
         ans = _quiz.ans;
     }
+
+    public override string ToString()
+    {
+        return (title + " " + "\nAnswer type: " + ansType);
+    }
+
 }
