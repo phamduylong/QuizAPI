ecc68f9473690ab5eba467d40000d16d6d854de5
longph <longphamduy2002@gmail.com>
2023-02-28 10:01:15 +0200

    update routes and use in memory db

--- Program.cs
+++ Program.cs
@@ -1,11 +1,12 @@
 ﻿using Newtonsoft.Json;
+using Microsoft.EntityFrameworkCore;
 Random rand = new Random();
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at
-// https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<QuizDb>(opt => opt.UseInMemoryDatabase("QuizList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -17,86 +18,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-
+// Redirect HTTP requests to HTTPS
 app.UseHttpsRedirection();
 
-var quizzes = new List<Quiz> { };
+app.MapGet("/quizzes", async (QuizDb db) => JsonConvert.SerializeObject(await db.Quizzes.ToListAsync()));
 
-var quiz1 = new Quiz("Who created Svelte?",
-    new List<string> { "Rich Harris", "Linus Torsvald",
-        "Mark Zuckerberg", "Ada Lovelace" }, AnswerType.str, "Rich Harris");
+app.MapGet("/quizzes/{id}", async (QuizDb db, int id) => JsonConvert.SerializeObject(await db.Quizzes.FindAsync(id)));
 
-var quiz2 = new Quiz("What version of Svelte are we in right now?", new List<string> { "0", "1", "2", "3" }, AnswerType.num, 3);
+app.MapPost("/quizzes", async (QuizDb db, Quiz quiz) => {
+    db.Quizzes.Add(quiz);
+    await db.SaveChangesAsync();
+    return Results.Created($"/quizzes/{quiz.Id}", quiz);
+});
 
-quizzes.Add(quiz1);
-quizzes.Add(quiz2);
+app.MapDelete("/quizzes/{id}", async (QuizDb db, int id) => {
+    if(await db.Quizzes.FindAsync(id) is Quiz quiz)
+    {
+        db.Quizzes.Remove(quiz);
+        await db.SaveChangesAsync();
+        return Results.Ok();
+    }
 
-app.MapGet("/quizzes", () =>
-{
-    var json_quiz = JsonConvert.SerializeObject(quizzes);
-    return json_quiz;
-})
-.WithName("quizzes");
-
-app.MapGet("/quizzes/random", () =>
-{
-    var json_quiz = JsonConvert.SerializeObject(quizzes[rand.Next(quizzes.Count)]);
-    return json_quiz;
-})
-.WithName("random");
+    return Results.NotFound();
+});
 
 app.Run();
 
-enum AnswerType
-{
-    str,
-    num,
-    binary
 
-}
 
-class Quiz
-{
-    // JSONProperty is need for serialization of private class members
-    [JsonProperty("title")]
-    private string title { get; set; }
-
-    [JsonProperty("options")]
-    private List<string> options { get; set; }
-
-    [JsonProperty("answertype")]
-    private AnswerType ansType { get; set; }
-
-    [JsonProperty("answer")]
-    private object ans { get; set; }
-
-    public Quiz()
-    {
-        title = "";
-        options = new List<string>();
-        ansType = AnswerType.str;
-        ans = "";
-    }
-
-    public Quiz(string _title, List<string> _options, AnswerType _ansType, object _ans)
-    {
-        title = _title;
-        options = _options;
-        ansType = _ansType;
-        ans = _ans;
-    }
-
-    public Quiz(Quiz _quiz)
-    {
-        title = _quiz.title;
-        options = _quiz.options;
-        ansType = _quiz.ansType;
-        ans = _quiz.ans;
-    }
-
-    public override string ToString()
-    {
-        return (title + " " + "\nAnswer type: " + ansType);
-    }
-
-}
